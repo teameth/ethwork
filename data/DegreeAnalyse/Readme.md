7fbed019bf7f24166af6ade6fc069aa6aa043936
PonderLY <1486342897@qq.com>
2019-03-26 17:10:06 +0800

    metrics

---
+++ data/DegreeAnalyse/Readme.md
@@ -0,0 +1,5 @@
+Construct a graph using networkx and conduct degree analysis on the graph.
+
+## Usage
+- python readgraphfromfile.py --input ../OpenNE/data/ethereum/CIGMFG --graph_type MFG --directed --weighted --label ../evaluate/data/labellist-22.csv
+- python readgraphfromfile.py --input ../OpenNE/data/ethereum/CIG --graph_type CIG --directed --weighted --label ../evaluate/data/labellist-22.csv
--- evaluation/evaluator.py
+++ evaluation/evaluator.py
@@ -8,14 +8,20 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 
 class QuantitativeEvaluator:
+    """
+    Analyse the embedding results.
+        :nt2vecs is a dict whose key is the node address and value is the embedding vector
+        :nt2type is a dict whose key is the node address and value is the node type, 'Sc' or 'No'
+        :query_neighbor is a dict whose key is the node address and value is a neighbor address list
+        :query_neighbor_dist is a dict whose key is the node address and value is a 2-tuple list,
+                                        the 2-tuple is like (address, distance)        
+    """
     def __init__(self, embed_file, label_file, node_file):
-        # self.embed_file = embed_file
-        # self.label_file = label_file
-        # self.node_file = node_file
         self.nt2vecs = dict()
         self.nt2type = dict()
         self.label_account = []
-        self.query_neighbor = dict() 
+        self.query_neighbor = dict()
+        self.query_neighbor_dist = dict()          
         self.read_embedding(embed_file)
         self.read_label(label_file)
         self.read_node_type(node_file)
@@ -63,15 +69,34 @@ class QuantitativeEvaluator:
         start_time = time.time()
         for query in queries:
             query_vec = self.nt2vecs[query]
-            sortMat = []
+            nbList = []
             for node in candidates:
                 node_vec = self.nt2vecs[node]
                 dist = np.linalg.norm(query_vec - node_vec)
                 if dist < 20:
-                    sortMat.append((node, dist))
-            self.query_neighbor[query] = sorted(sortMat, key=lambda x: x[1])
+                    nbList.append((node, dist))
+            nbSort = sorted(nbList, key=lambda x: x[1])
+            self.query_neighbor[query] = [tup[0] for tup in nbSort]
+            self.query_neighbor_dist[query] = nbSort
         print('Neighbors found for {} queries, elapsed time {}s'.format(len(queries), time.time()-start_time))
 
+
+    def get_metrics(self, k=10):
+        p_k = []
+        r_k = []
+        label = set(self.label_account)
+        for query in self.query_neighbor.keys():
+            nb = self.query_neighbor[query]
+            if len(nb) > k:
+                nb = nb[:k]
+            interSet = set(nb) & label
+            p_k.append(len(interSet)/len(nb))
+            r_k.append(len(interSet)/len(label))
+        precision = sum(p_k)/len(p_k)
+        recall = sum(r_k)/len(r_k)
+        print("Precision@{}={}".format(k, precision))
+        print("Recall@{}={}".format(k, recall))  
+            
 
     def write_neighbors(self, out_path, k=10):
         fw = open(out_path, 'w')
@@ -103,6 +128,8 @@ if __name__ == "__main__":
     args = parse_args()
     evaluator = QuantitativeEvaluator(args.embedding, args.label, args.node_type)
     evaluator.write_neighbors(args.output)
+    evaluator.get_metrics(10)
+    evaluator.get_metrics(20)    
     
 
 
