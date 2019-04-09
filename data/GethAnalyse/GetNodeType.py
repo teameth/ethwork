c7113748fce1a3ca86a7a6724c4e81280e0b7d5d
PonderLY <1486342897@qq.com>
2019-04-09 21:19:00 +0800

    read mutiple dict files

--- data/GethAnalyse/GetNodeType.py
+++ data/GethAnalyse/GetNodeType.py
@@ -38,13 +38,11 @@ def getTypefromList(addrTupleList):
             flag = 'sc' if len(sc_code) > 2 else 'eoa'
             results.append((addrTuple[0], addrTuple[1], flag))
     return results
-    
- 
 
-if __name__ == '__main__':
-    node_dict = '/home/liuy/Documents/qinz/dict' if len(sys.argv) <= 1 else sys.argv[1]    
+
+def read_dict(file_path):
     start_time = time.time()
-    dict_file = open(node_dict, 'r')
+    dict_file = open(file_path, 'r')
     dict_reader = csv.reader(dict_file)
     line_num = 0
     query_num = 10000
@@ -56,17 +54,22 @@ if __name__ == '__main__':
         #     break
     dict_file.close()
     print("{} lines read done, elapsed time {}".format(len(addrTupleList), time.time()-start_time))
-    
+    return addrTupleList
+
+
+def getTypeParellel(addrTupleList, pool_num=10):
     start_time = time.time()
-    pool = Pool(20)  
+    pool = Pool(pool_num)  
     nodeTypeList = pool.map(getType, addrTupleList)  
     pool.close()
     pool.join()
-    # nodeTypeList = getTypefromList(addrTupleList)
-    print("Query done, elapsed time {}s".format(time.time()-start_time))   
+    print("Query done, elapsed time {}s".format(time.time()-start_time)) 
+    return nodeTypeList
 
+
+def write_dict(out_path, nodeTypeList):
     start_time = time.time()
-    out_file = open('/home/liuy/Documents/qinz/node_dict', 'w')    
+    out_file = open(out_path, 'a')    
     dict_writer = csv.writer(out_file) 
     for nt in nodeTypeList: 
         dict_writer.writerow(list(nt))
@@ -74,6 +77,14 @@ if __name__ == '__main__':
     print("{} lines written done, elapsed time {}".format(len(nodeTypeList), time.time()-start_time))
     
 
+
+if __name__ == '__main__':
+    node_dict = './data/node_dict/part_' if len(sys.argv) <= 1 else sys.argv[1]    
+    for i in ['07', '08', '09', '10', '11', '12']:
+        addrTupleList = read_dict(node_dict+i)
+        # nodeTypeList = getTypefromList(addrTupleList)
+        nodeTypeList = getTypeParellel(addrTupleList)
+        write_dict('./data/node_dict/node_type_dict', nodeTypeList)
 
 	
 			
